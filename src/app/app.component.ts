d2f22f4f1b7bc736ee0db322ea123b6e126a7e26
Diego Amancio <diego.pereira@ccc.ufcg.edu.br>
2024-02-25 13:21:07 -0300

    feat: add filter, see more

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { RouterOutlet } from "@angular/router";
 import { Employee, EmployeeCardsComponent } from "./components/molecules/employee-cards/employee-cards.component";
 import { getEmployees } from "./api/employee";
 import { FloatingButtonComponent } from "./components/molecules/floating-button/floating-button.component";
+import { MatInputModule } from "@angular/material/input";
+import { FormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
 
 
 @Component({
@@ -11,16 +14,22 @@ import { FloatingButtonComponent } from "./components/molecules/floating-button/
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, EmployeeCardsComponent, CommonModule, FloatingButtonComponent]
+  imports: [RouterOutlet, EmployeeCardsComponent, CommonModule, FloatingButtonComponent, MatInputModule, FormsModule, MatButtonModule],
+
+
 })
 export class AppComponent {
   title = 'serverless-challenge-frontend';
   employees: Employee[] = [];
+  inputValue = ''
+  filteredEmployees: Employee[] = []
+
 
   constructor() {
     this.deleteEmployeeFromList = this.deleteEmployeeFromList.bind(this)
     this.addEmployeeInList = this.addEmployeeInList.bind(this)
   }
+
   deleteEmployeeFromList(deletedEmployee: Employee) {
     this.employees = this.employees.filter(({ Id }) => Id !== deletedEmployee.Id)
   }
@@ -30,22 +39,26 @@ export class AppComponent {
 
   getLastId = () => {
     const employeesAux = Object.assign([], this.employees)
-    const lastItem = employeesAux.pop()
+    const lastemployee = employeesAux.pop()
 
-    if (lastItem !== undefined) {
-      return lastItem['Id']
+    if (lastemployee !== undefined) {
+      return lastemployee['Id']
     }
 
     return undefined
   }
-  getEmployeesHandle(limit = 10) {
+
+  filterEmployeeByInput = (filterValue: any) => {
+    const searchvalue: string = filterValue.target.value.toLocaleLowerCase();
+    this.filteredEmployees = searchvalue === '' ? this.employees : this.employees.filter(({ Nome }) => Nome.toLowerCase().includes(searchvalue))
+  }
+  getEmployeesHandle(limit = 2) {
     getEmployees({ limit: limit, lastIdFromList: this.getLastId() }).then(({ data }) => {
-      this.employees = data
+      this.employees = this.employees.concat(data)
+      this.filteredEmployees = this.employees
     })
   }
   ngOnInit() {
-
     this.getEmployeesHandle()
-
   }
 }
